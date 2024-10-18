1a2e37050493fe7b5da1c9bc3ce63805e897a4ce
kulotskiI <ishielmishma24@gmail.com>
2024-10-18 20:02:23 +0800

    Initial commit

---
+++ arrayActivity.js
@@ -0,0 +1,10 @@
+const faveSinger = ["Ben and ben", "Parokia ni Edgar", "Moria"];
+console.log("First singer: ", faveSinger[2]);
+
+const faveNumber = ["10", "24", "19","27"];
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log("First item in mixedArr:", mixedArr[0]);
+console.log("Second item in mixedArr:", mixedArr[1]);
+console.log("Third item in mixedArr:", mixedArr[2]);
+console.log("Fourth item in mixedArr:", mixedArr[3]);
